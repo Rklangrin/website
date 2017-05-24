29704e89f6dc7f1b516f9fe5bc8b4bfda28b221b
Rklangrin <rklandgren@gmail.com>
2017-05-23 21:07:28 -0500

    creates tab layout

--- js/application.js
+++ js/application.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  console.log("hello world")
+  
+  $("a.about-tabs").click(function(){
+    event.preventDefault();
+    var id = $(this).html();
+
+    var paragraphsArr = $("#about div");
+    var tabTitles = $("a.about-tabs");
+
+    for (var i = 0; i < paragraphsArr.length; i++) {
+      if($(paragraphsArr[i]).attr("id") === id){
+        $(paragraphsArr[i]).show();
+        $(tabTitles[i]).css("color", "white");
+      } else {
+        $(paragraphsArr[i]).hide();
+        $(tabTitles[i]).css("color", "black");
+      }
+    }
+  })
+
 });
